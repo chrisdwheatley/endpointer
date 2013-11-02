12ff9fc920f212e4ca72fa0e2510ba399dd5581b
Chris Wheatley <chriswheatley87@gmail.com>
2013-11-02 21:55:03 +0000

    now creates directories where they aren't defined and writes files once dir is created

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var fs = require('fs');
+var path = require('path');
+var mkdirp = require('mkdirp');
 
 var app = express();
 
@@ -11,7 +13,7 @@ app.use(express.bodyParser());
 
 app.get('/', function(req, res) {
   var body = '<form action="/" method="post">' + 'path <input type="text" id="path" name="path"></input>' + 'json <input type="text" id="json" name="json"></input>' + '<input type="submit" value="submit"></input>' + '</form>';
-  
+
   res.setHeader('Content-Type', 'text/html; charset=utf-8');
   res.setHeader('Content-Length', body.length);
 
@@ -21,13 +23,25 @@ app.get('/', function(req, res) {
 app.post('/', function(req, res) {
   res.redirect('/');
 
-  var path = req.body.path;
-  var fullPath = 'endpoints/' + path;
+  var input = req.body.path;
+  var file = path.basename(input);
+  var endpointPath = 'endpoints/' + input;
+  var dir = path.dirname(endpointPath);
+
   var json = req.body.json;
 
-  fs.writeFile(fullPath, json, function(err) {
-    if (err) throw err;
-    console.log('Saved to %s', fullPath);
+  mkdirp(dir, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      fs.writeFile(endpointPath, json, function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('%s Saved to %s', file, dir);
+        }
+      });
+    }
   });
 
   res.end();
